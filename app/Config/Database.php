356bc1ab2187cd2388fbba13582f47315e22c792
BJ505 <byronjaramilloquezada@gmail.com>
2024-11-08 00:13:08 -0300

    add Database script
    se añade script para ejecutar en localhost

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'tarea_solutoria',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
